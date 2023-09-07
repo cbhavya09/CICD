2e33e006f721fc6003258ea8f0a94dae7f7760f6
cbhavya09 <c.bhavya@sonata-software.com>
2023-09-07 16:14:35 +0530

    commit

--- src/main/java/com/example/HelloWorldServlet.java
+++ src/main/java/com/example/HelloWorldServlet.java
@@ -3,7 +3,7 @@ package com.example;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.IOException;
-import java.io.PrintWriter; // Add this import statement
+import java.io.PrintWriter; // Add this import statements
 
 public class HelloWorldServlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response)
